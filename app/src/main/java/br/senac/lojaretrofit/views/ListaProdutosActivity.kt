b5672cf902df5e5130d3a385def622b0a63098f9
jose.csilva87 <josecarloos901@gmail.com>
2022-05-24 22:28:56 -0300

    aula de API

--- app/src/main/java/br/senac/lojaretrofit/views/ListaProdutosActivity.kt
+++ app/src/main/java/br/senac/lojaretrofit/views/ListaProdutosActivity.kt
@@ -1,8 +1,17 @@
 package br.senac.lojaretrofit.views
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import br.senac.lojaretrofit.databinding.ActivityListaProdutosBinding
+import br.senac.lojaretrofit.databinding.CardItemBinding
+import br.senac.lojaretrofit.model.Produto
+import br.senac.lojaretrofit.services.API
+import com.google.android.material.snackbar.Snackbar
+import com.squareup.picasso.Picasso
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ListaProdutosActivity : AppCompatActivity() {
     lateinit var binding: ActivityListaProdutosBinding
@@ -11,6 +20,66 @@ class ListaProdutosActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityListaProdutosBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        listaProdutos()
+    }
+
+    fun listaProdutos(){
+
+        val callback = object : Callback<List<Produto>> {
+
+            override fun onResponse(call: Call<List<Produto>>, response: Response<List<Produto>>) {
+                if(response.isSuccessful){
+                    val listaProdutos = response.body()
+                    atualizarTela(listaProdutos)
+                }
+                else{
+                    Snackbar.make(binding.container,
+                        "Nao foi possivel conectar ao servidor",
+                        Snackbar.LENGTH_LONG)
+                        .show()
+                }
+                binding.progressBar.visibility = View.INVISIBLE
+            }
+            override fun onFailure(call: Call<List<Produto>>, t: Throwable) {
+                Snackbar.make(binding.container,
+                    "Nao foi possivel conectar ao servidor",
+                    Snackbar.LENGTH_LONG)
+                    .show()
+                binding.progressBar.visibility = View.INVISIBLE
+            }
+
+
+        }
+
+
+        API.produto.listar().enqueue(callback)
+        binding.progressBar.visibility = View.VISIBLE
+    }
+
+
+    fun atualizarTela(listaProdutos: List<Produto>?){
+        binding.container.removeAllViews()
+
+        listaProdutos?.forEach {
+
+            val cardBinding = CardItemBinding.inflate(layoutInflater)
+
+            cardBinding.textNome.text = it.nomeProduto
+            cardBinding.textPreco.text = it.precProduto.toString()
+
+            Picasso
+                .get()
+                .load("https://oficinacordova.azurewebsites.net/android/rest/produto/image/${it.idProduto}")
+                .into(cardBinding.imagem)
+
+            binding.container.addView(cardBinding.root)
+
+        }
     }
 
 }
